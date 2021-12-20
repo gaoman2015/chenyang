adb7ab14c97217dc6d543bab1a9769cf2cf75ba9
高曼 <elliegmgao@tencent.com>
2021-12-20 11:57:09 +0800

    fix: 修改路由

---
+++ src/components/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+     <div>欢迎来到河北宸洋机械零部件加工有限公司</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Root',
+  data () {
+    return {}
+  },
+  mounted () {}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
