4abf24fe898e01e4dc58f997156aa5ee195d068b
高曼 <elliegmgao@tencent.com>
2021-12-22 10:54:58 +0800

    feat: 增加header

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-     <div>欢迎来到河北宸洋机械零部件加工有限公司</div>
+  <div class="container">
+     <div class="header">
+       <div class="left">
+         <span class="logo">CYJX</span>
+         <span>河北宸洋机械</span>
+        </div>
+       <div class="center">1111</div>
+       <div class="right">22222</div>
+      </div>
   </div>
 </template>
 
@@ -16,4 +23,33 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.container {
+  background-color: #f2f2f2;
+}
+.header {
+  background-color: #46aade;
+  height: 120px;
+  padding: 10px 50px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+.left {
+  font-size: 26px;
+  color: #fff;
+  font-weight: 700;;
+}
+.logo {
+  background-color: #000;
+  background-image: url('../assets/jx.png');
+  background-repeat: no-repeat;
+  background-origin: content-box;
+  background-position: center;
+  width: 20px;
+  height: 20px;
+  font-weight: 700;
+  padding: 10px;
+  border-radius: 10px;
+}
 </style>
